cfa8d24964b982b71475dae03b808e453d413f92
ishansh1200 <ishansh1200@gmail.com>
2025-04-24 14:31:28 +0530

    DEPLOYMENT FIX

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
-const withPWA = require("next-pwa")({
+/**
+ * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially useful
+ * for Docker builds.
+ */
+await import("./src/env.js");
+import WithPWA from "next-pwa";
+
+const withPWA = WithPWA({
   dest: "public",
   disable: process.env.NODE_ENV === "development",
   register: true,
@@ -9,15 +16,19 @@ const withPWA = require("next-pwa")({
 /**
  * @type {import('next').NextConfig}
  */
+// @ts-ignore
 const config = withPWA({
-  output: "export", // Ensure static export
   reactStrictMode: true,
 
-  // Only use if you're not using the new app directory
+  /**
+   * If you are using `appDir` then you must comment the below `i18n` config out.
+   *
+   * @see https://github.com/vercel/next.js/issues/41980
+   */
   i18n: {
     locales: ["en"],
     defaultLocale: "en",
   },
 });
 
-module.exports = config;
+export default config;
